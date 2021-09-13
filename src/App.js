cd3637ac0b49f304200899b39872b3014451979a
Hassan Baig <hassan@mzwebstudio.com>
2021-09-13 20:24:25 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
 
 function App() {
+
+  const [tasks, settasks] = useState([
+        {
+          "id": 1,
+          "text": "Doctors Appointment",
+          "day": "Feb 5th at 2:30pm",
+          "reminder": true,
+          "isActive" : true
+        },
+        {
+          "id": 2,
+          "text": "Meeting at School",
+          "day": "Feb 6th at 1:30pm",
+          "reminder": true,
+          "isActive": true
+        }
+      ])
+    
+  const addTask = (task)=>{
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTasks = {id , ...task};
+    settasks([...tasks , newTasks]);
+   
+  }
+
+  const deleteTask = (id) =>{
+    settasks(tasks.map((task)=>task.id ===id ? {...tasks, isActive:false}:task))
+    settasks(tasks.filter((task)=> task.id!==id));
+  }
+  const Reminder = (id)=>{
+    settasks(tasks.map((task)=>task.id === id ? {...task, reminder:!task.reminder}:task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Header title="Todo List"/>
+    <AddTask onAdd={addTask}/>
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={Reminder}/> : 'Nothing to show'}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default Header
